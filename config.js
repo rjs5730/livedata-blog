fe451ca6a3aaf50215c3c01cc36ab546fd34d968
livedata <rjs5730@naver.com>
2021-06-21 20:15:43 +0900

    fix: config 파일 깃 허브 주소 수정

--- config.js
+++ config.js
@@ -1,54 +1,54 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://rjs5730.github.io/livedata-blog/',
-  pathPrefix: '/livedata-blog',
-  title: 'Seunhyun blog',
+  url: "https://rjs5730.github.io/livedata-blog/",
+  pathPrefix: "/livedata-blog",
+  title: "Seunhyun blog",
   subtitle: `웹 프론트 개발자.`,
-  copyright: '© All rights reserved.',
-  disqusShortname: 'livedata-1',
+  copyright: "© All rights reserved.",
+  disqusShortname: "livedata-1",
   postsPerPage: 5,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'All Posts',
-      path: '/'
+      label: "All Posts",
+      path: "/",
     },
     {
-      label: 'Categories',
-      path: '/categories'
+      label: "Categories",
+      path: "/categories",
     },
     {
-      label: 'Tags',
-      path: '/tags'
+      label: "Tags",
+      path: "/tags",
     },
     {
-      label: 'My Info',
-      path: '/pages/about'
+      label: "My Info",
+      path: "/pages/about",
     },
   ],
   author: {
-    name: 'Oh Seung-hyun',
-    photo: '/photo.png',
+    name: "Oh Seung-hyun",
+    photo: "/photo.png",
     bio: `웹 프론트 개발자.`,
     contacts: {
-      email: 'rjs5730@naver.com',
-      facebook: '',
-      telegram: '',
-      twitter: '',
-      github: 'github.com/rjs5730',
-      rss: '',
-      vkontakte: '',
-      linkedin: '',
-      instagram: '',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-      medium: '',
-    }
-  }
+      email: "rjs5730@naver.com",
+      facebook: "",
+      telegram: "",
+      twitter: "",
+      github: "rjs5730",
+      rss: "",
+      vkontakte: "",
+      linkedin: "",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      medium: "",
+    },
+  },
 };
